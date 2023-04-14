21193b88b16d2bb4f2e8480319cc11b0d433ec5b
Devshara <developer.ishara@gmail.com>
2023-04-14 03:39:31 +0100

    complete 4 projects

---
+++ projects/04/Sum1ToN.asm
@@ -0,0 +1,52 @@
+//  Program: Sum1ToN
+//  Computes RAM[1] = 1+2+3+...+RAM[0]
+//  Usage: put a value >= 1 in RAM[0]
+
+//  i = 1
+//  sum = 0
+// LOOP:
+//  if(i > R0) goto STOP
+//  sum = sum+i
+//  i = i+1
+//  goto LOOP
+// STOP:
+//  R1 = sum 
+
+    //i = 1
+    @i
+    M=1
+    // sum = 0
+    @sum
+    M=0
+(LOOP)
+    //if(i > R0) goto STOP
+    @i
+    D=M
+    @R0
+    D=D-M
+    @STOP
+    D;JGT
+    //sum=sum+i
+    @sum
+    D=M
+    @i
+    D=D+M
+    @sum
+    M=D 
+    // i = i+1
+    @i
+    M=M+1
+    //goto LOOP
+    @LOOP
+    0;JMP
+(STOP)
+    @sum
+    D=M
+    @R1
+    M=D
+(END)
+    @END
+    0;JMP
+
+
+
